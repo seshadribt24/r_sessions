7931d407eb6afec6fbc74d411783c4a88f1f379b
seshadribt24 <seshadribt24@gmail.com>
2016-10-18 18:28:31 -0700

    practices

---
+++ day1_vectors.R
@@ -0,0 +1,14 @@
+vector_1 = c(1,2,3)
+vector_1
+seq=1:10
+seq # this is different from the below one
+seq(1,10,1) #this is to create a matrix with start, end and increment
+
+
+#naming vectors
+one=c(1,2,3)
+two=c("one", "two", "three")
+names(one) = two
+one
+
+#vector functions
