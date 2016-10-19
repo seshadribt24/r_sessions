ea078d314d9106741e9e6d7eb9fa5cc81f36137f
seshadribt24 <seshadribt24@fb.com>
2016-10-18 18:53:25 -0700

    lesson continued on work computer

--- day1_vectors.R
+++ day1_vectors.R
@@ -14,3 +14,17 @@ one
 #vector functions
 #1. selecting values
 one[1:2] #vectors here are indexed from 1
+one[c(1,3)]
+one[-1]
+one["one"]
+one[one>1]
+
+#2. more functions
+x=c(3,4,7)
+x+3 #adds 3 to each element in the vector
+z = c(1.5, 1/6, 1/3)
+round(z, 2) #rounds each element in the matrix to 2 decimal places
+sort(z)
+length(z)
+max(z)
+mean(z)
